505fab2b67fba61dce6863e00b014555d534f77f
Menarul Alam <menarulalam@gmail.com>
2021-05-18 22:50:33 -0400

    tried to imlplement accounts for vec

--- lang/src/vec.rs
+++ lang/src/vec.rs
@@ -1,6 +1,8 @@
-use crate::{ToAccountInfos, ToAccountMetas};
+use crate::{Accounts, ToAccountInfos, ToAccountMetas};
 use solana_program::account_info::AccountInfo;
 use solana_program::instruction::AccountMeta;
+use solana_program::program_error::ProgramError;
+use solana_program::pubkey::Pubkey;
 
 impl<'info, T: ToAccountInfos<'info>> ToAccountInfos<'info> for Vec<T> {
     fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
@@ -15,5 +17,19 @@ impl<T: ToAccountMetas> ToAccountMetas for Vec<T> {
         self.iter()
             .flat_map(|item| (*item).to_account_metas(is_signer))
             .collect()
+    }
+}
+
+impl<'info, T: Accounts<'info>> Accounts for Vec<T> {
+    fn try_accounts(
+                    _program_id: &Pubkey,
+                    accounts: &mut Vec<T>,
+    ) -> Result<Self, ProgramError> {
+        if accounts.is_empty() {
+            return Err(ProgramError::NotEnoughAccountKeys);
+        }
+        let account = &accounts[0];
+        accounts.drain(0);
+        Ok(vec![account.clone()])
     }
 }
